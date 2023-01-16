5939bc4ea9f0ea16ebbc6fc6045213db1047b7cd
Erick Giorgio <erick.silva@hotmart.com>
2023-01-16 13:33:54 -0300

    Create project

---
+++ src/main/kotlin/com/springjwt/springboot3jwt/Springboot3JwtApplication.kt
@@ -0,0 +1,11 @@
+package com.springjwt.springboot3jwt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Springboot3JwtApplication
+
+fun main(args: Array<String>) {
+    runApplication<Springboot3JwtApplication>(*args)
+}
---
+++ src/test/kotlin/com/springjwt/springboot3jwt/Springboot3JwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.springjwt.springboot3jwt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Springboot3JwtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
